2b02f2a71997efbafea06fb9a2c33b973fdfdaeb
radman <wesleychase1@live.com>
2023-12-09 16:55:02 +0000

    Added the ability to enter commands in the console

--- RobotSimulator.App/Program.cs
+++ RobotSimulator.App/Program.cs
@@ -8,12 +8,29 @@ var board = new Board(5);
 var robot = new Robot(board);
 Thread.Sleep(1500);
 Console.WriteLine("Robot booted up successfully!");
+
 Console.WriteLine("Downloading commands...");
-Thread.Sleep(1500);
-var commandsAsString = await File.ReadAllLinesAsync("commands.txt");
+string?[] commandsAsString = await File.ReadAllLinesAsync("commands.txt");
 var commandQueue = CommandParser.Parse(commandsAsString);
 Console.WriteLine("Commands downloaded successfully!");
+
 Console.WriteLine("Executing commands...");
-Thread.Sleep(2000);
 robot.Execute(commandQueue);
 Console.WriteLine("Commands executed successfully!");
+
+Thread.Sleep(1500);
+while(true)
+{
+    Console.WriteLine("Enter a command or type 'exit' to quit:");
+    var command = Console.ReadLine();
+    if (command != null && 
+        command.Equals("exit", StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+    commandQueue = CommandParser.Parse(new[]{command});
+    robot.Execute(commandQueue);
+    Thread.Sleep(1500);
+}
+Console.WriteLine("Robot shutting down...");
+Thread.Sleep(1500);
--- RobotSimulator.App/RobotCommands/CommandParser.cs
+++ RobotSimulator.App/RobotCommands/CommandParser.cs
@@ -4,33 +4,33 @@ namespace RobotSimulator.App.RobotCommands;
 
 public static class CommandParser
 {
-    public static Queue<IRobotCommand> Parse(string[] commandsAsStrings)
+    public static Queue<IRobotCommand> Parse(string?[] commandsAsStrings)
     {
         var commandQueue = new Queue<IRobotCommand>();
-        
+
         foreach (var commandAsString in commandsAsStrings)
         {
             Thread.Sleep(100);
             Console.WriteLine($"Parsing command: {commandAsString}");
             switch (commandAsString)
             {
-                case not null when commandAsString.StartsWith("PLACE"):
+                case not null when commandAsString.StartsWith("PLACE", StringComparison.OrdinalIgnoreCase):
                     var placeCommand = PlaceCommand.Parse(commandAsString);
                     commandQueue.Enqueue(placeCommand);
                     break;
-                case not null when commandAsString.StartsWith("MOVE"):
+                case not null when commandAsString.StartsWith("MOVE", StringComparison.OrdinalIgnoreCase):
                     var moveCommand = new MoveCommand();
                     commandQueue.Enqueue(moveCommand);
                     break;
-                case not null when commandAsString.StartsWith("LEFT"):
+                case not null when commandAsString.StartsWith("LEFT", StringComparison.OrdinalIgnoreCase):
                     var rotateLeftCommand = new RotateCommand(RotationDirection.Left);
                     commandQueue.Enqueue(rotateLeftCommand);
                     break;
-                case not null when commandAsString.StartsWith("RIGHT"):
+                case not null when commandAsString.StartsWith("RIGHT", StringComparison.OrdinalIgnoreCase):
                     var rotateRightCommand = new RotateCommand(RotationDirection.Right);
                     commandQueue.Enqueue(rotateRightCommand);
                     break;
-                case not null when commandAsString.StartsWith("REPORT"):
+                case not null when commandAsString.StartsWith("REPORT", StringComparison.OrdinalIgnoreCase):
                     var reportCommand = new ReportCommand();
                     commandQueue.Enqueue(reportCommand);
                     break;
