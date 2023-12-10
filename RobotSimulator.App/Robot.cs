12b80546c78fcb0c2262444659fbc99a6b1ffffb
radman <wesleychase1@live.com>
2023-12-10 11:40:41 +0000

    Extracted command logic out into their own methods to simplify execute method and keep command behaviour isolated

--- RobotSimulator.App/Robot.cs
+++ RobotSimulator.App/Robot.cs
@@ -18,74 +18,95 @@ public class Robot(Board board)
             switch (command)
             {
                 case PlaceCommand placeCommand:
-                    if (board.IsPositionValid(placeCommand.Position))
-                    {
-                        Position = placeCommand.Position;
-                        FacingDirection = placeCommand.FacingDirection;
-                        _hasBeenPlaced = true;
-                    }
+                    ExecutePlaceCommand(board, placeCommand);
 
                     break;
                 case RotateCommand rotateCommand:
-                    if (_hasBeenPlaced)
-                    {
-                        FacingDirection = Rotate(rotateCommand.RotationDirection);
-                    }
-                    else
-                    {
-                        LogRobotNotPlaced(rotateCommand, $" Direction: {rotateCommand.RotationDirection}");
-                    }
+                    ExecuteRotateCommand(rotateCommand);
 
                     break;
                 case MoveCommand moveCommand:
-                    if (_hasBeenPlaced)
-                    {
-                        var movementVector = FacingDirection switch
-                        {
-                            FacingDirection.North => new Vector2(0, 1),
-                            FacingDirection.East => new Vector2(1, 0),
-                            FacingDirection.South => new Vector2(0, -1),
-                            FacingDirection.West => new Vector2(-1, 0),
-                            _ => throw new ArgumentOutOfRangeException()
-                        };
-
-                        var newPosition = Position + movementVector;
-                        if (board.IsPositionValid(newPosition))
-                        {
-                            Position = newPosition;
-                        }
-                    }
-                    else
-                    {
-                        LogRobotNotPlaced(moveCommand);
-                    }
+                    ExecuteMoveCommand(board, moveCommand);
 
                     break;
                 case ReportCommand reportCommand:
-                    if(_hasBeenPlaced)
-                    {
-                        Console.WriteLine($"Robot Position: ( {Position.X} , {Position.Y} ). " +
-                                          $"Robot Facing: {FacingDirection}");
-                    }
-                    else
-                    {
-                        LogRobotNotPlaced(reportCommand);
-                    }
+                    ExecuteReportCommand(reportCommand);
                     break;
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(command));
+                default:
+                    Console.WriteLine($"Command not recognised: {command.GetType().Name}");
+                    break;
             }
+        }
+    }
+
+    private void ExecuteReportCommand(ReportCommand reportCommand)
+    {
+        if (_hasBeenPlaced)
+        {
+            Console.WriteLine($"Robot Position: ( {Position.X} , {Position.Y} ). " +
+                              $"Robot Facing: {FacingDirection}");
+        }
+        else
+        {
+            LogRobotNotPlaced(reportCommand);
+        }
+    }
+
+    private void ExecuteMoveCommand(Board board, MoveCommand moveCommand)
+    {
+        if (_hasBeenPlaced)
+        {
+            var movementVector = FacingDirection switch
+            {
+                FacingDirection.North => new Vector2(0, 1),
+                FacingDirection.East => new Vector2(1, 0),
+                FacingDirection.South => new Vector2(0, -1),
+                FacingDirection.West => new Vector2(-1, 0),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            var newPosition = Position + movementVector;
+            if (board.IsPositionValid(newPosition))
+            {
+                Position = newPosition;
+            }
+        }
+        else
+        {
+            LogRobotNotPlaced(moveCommand);
+        }
+    }
+
+    private void ExecuteRotateCommand(RotateCommand rotateCommand)
+    {
+        if (_hasBeenPlaced)
+        {
+            FacingDirection = GetFacingDirectionAfterRotation(rotateCommand.RotationDirection);
+        }
+        else
+        {
+            LogRobotNotPlaced(rotateCommand, $" Direction: {rotateCommand.RotationDirection}");
+        }
+    }
+
+    private void ExecutePlaceCommand(Board board, PlaceCommand placeCommand)
+    {
+        if (board.IsPositionValid(placeCommand.Position))
+        {
+            Position = placeCommand.Position;
+            FacingDirection = placeCommand.FacingDirection;
+            _hasBeenPlaced = true;
         }
     }
 
     private static void LogRobotNotPlaced(IRobotCommand discardedCommand, string additionalInfo = "")
     {
-        Console.WriteLine($"Robot has not been placed yet. " +
+        Console.Error.WriteLine($"Robot has not been placed yet. " +
                           $"Discarding command: {discardedCommand.GetType().Name}." +
                           $"{additionalInfo}");
     }
 
-    private FacingDirection Rotate(RotationDirection rotationDirection)
+    private FacingDirection GetFacingDirectionAfterRotation(RotationDirection rotationDirection)
     {
         var newRotation = ((int)FacingDirection + (int)rotationDirection) % 4;
         return FacingDirection = newRotation < 0 ? (FacingDirection)3 : (FacingDirection)newRotation;
